de78a084c4076697b03307b7da6f72dca65af7d0
Walid ATHMANI <athmani.walid@icloud.com>
2026-02-05 22:12:34 +0100

    add menu tables in menu.ts and list of menu in menu.astro

---
+++ src/data/menu.ts
@@ -0,0 +1,109 @@
+export const menuItems = [
+    {
+      id: "tacos-gratine",
+      name: "Tacos Gratiné",
+      description: "Sauce fromagère, frites, gratiné au four",
+      fillings: [
+        {
+          id: "poulet",
+          name: "Poulet",
+          sizes: [
+            { size: "L", price: 400 },
+            { size: "XL", price: 800 },
+            { size: "XXL", price: 1200 },
+          ],
+        },
+        {
+          id: "viande-hachee",
+          name: "Viande hachée",
+          sizes: [
+            { size: "L", price: 450 },
+            { size: "XL", price: 900 },
+            { size: "XXL", price: 1350 },
+          ],
+        },
+        {
+            id: "abats",
+            name: "Abats",
+            sizes: [
+              { size: "L", price: 400 },
+              { size: "XL", price: 800 },
+              { size: "XXL", price: 1200 },
+            ],
+          },
+          {
+            id: "merguez",
+            name: "Merguez",
+            sizes: [
+              { size: "L", price: 400 },
+              { size: "XL", price: 800 },
+              { size: "XXL", price: 1200 },
+            ],
+          },
+          {
+            id: "mixte",
+            name: "Mixte",
+            sizes: [
+              { size: "L", price: 500 },
+              { size: "XL", price: 1000 },
+              { size: "XXL", price: 1500 },
+            ],
+          },
+      ],
+    },
+
+    {
+
+    id: "tacos",
+      name: "Tacos",
+      description: "Tacos classique, sauce fromagère, frites",
+      fillings: [
+        {
+          id: "poulet",
+          name: "Poulet",
+          sizes: [
+            { size: "L", price: 350 },
+            { size: "XL", price: 700 },
+            { size: "XXL", price: 1050 },
+          ],
+        },
+        {
+          id: "viande-hachee",
+          name: "Viande hachée",
+          sizes: [
+            { size: "L", price: 400 },
+            { size: "XL", price: 800 },
+            { size: "XXL", price: 1200 },
+          ],
+        },
+        {
+            id: "abats",
+            name: "Abats",
+            sizes: [
+              { size: "L", price: 350 },
+              { size: "XL", price: 700 },
+              { size: "XXL", price: 1050 },
+            ],
+          },
+          {
+            id: "merguez",
+            name: "Merguez",
+            sizes: [
+              { size: "L", price: 350 },
+              { size: "XL", price: 700 },
+              { size: "XXL", price: 1050 },
+            ],
+          },
+          {
+            id: "mixte",
+            name: "Mixte",
+            sizes: [
+              { size: "L", price: 500 },
+              { size: "XL", price: 1000 },
+              { size: "XXL", price: 1500 },
+            ],
+          },
+      ],
+    },
+  ];
+  
